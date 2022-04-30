b15dff1f03934d080fd7303baf335d176947774d
Maxwell Douglas <jmdougla@ualberta.ca>
2022-04-29 18:33:06 -0700

    Add genes-per-bin, and samplenames-per-gain/loss to output table

--- scripts/makeFilteredCNSummaryTable.R
+++ scripts/makeFilteredCNSummaryTable.R
@@ -2,6 +2,9 @@
 # Then convert this table into a useable/helpful output format for wetlab researchers.
 
 suppressPackageStartupMessages({
+  library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+  library(org.Hs.eg.db)
+  library(annotate)
   library(stringr)
   library(dplyr)
   library(tidyr)
@@ -34,6 +37,8 @@ collapse_table_to_segments <- function(df) {
       loss_probability = dplyr::first(loss.freq),
       gain_count = dplyr::first(gain.count),
       loss_count = dplyr::first(loss.count),
+      gain_samples = dplyr::first(gain.samples),
+      loss_samples = dplyr::first(loss.samples),
       bin_count = n(),
       sum_of_bin_lengths = sum(length)
     )
@@ -55,6 +60,7 @@ make_summary_table <- function(CNobj, snames, lowT, highT, pL, pG, prop, save_pa
   
   uni.chrom <- unique(df$chrom)
   
+  # Add gain / loss metrics per bin to output table
   nclass <-3
   if (!is.null(probamp(CNobj))) nclass <- nclass+1 
   if (!is.null(probdloss(CNobj))) nclass <- nclass+1 
@@ -66,26 +72,59 @@ make_summary_table <- function(CNobj, snames, lowT, highT, pL, pG, prop, save_pa
   bin_cns_gain <- as.data.frame(cns > highT) %>% dplyr::mutate(total = rowSums(across(where(is.logical))))
   df$loss.count <- bin_cns_loss$total
   df$gain.count <- bin_cns_gain$total
-  browser()
+  
+  # Add per-gain or loss sample names to output table
+  mat <- as.data.frame(t(colnames(bin_cns_loss)[1:dim(cns)[2]])) %>% dplyr::slice(rep(1:n(), each = dim(bin_cns_loss)[1]))
+  mat <- as.matrix(mat)
+  mask <- as.matrix(bin_cns_loss[,c(1:dim(cns)[2])])
+  mat[!mask] <- NA
+  mat <- as.data.frame(mat) %>% tidyr::unite("names", everything(), sep = ',', na.rm = TRUE)
+  df$loss.samples <- mat$names
+  mat <- as.data.frame(t(colnames(bin_cns_gain)[1:dim(cns)[2]])) %>% dplyr::slice(rep(1:n(), each = dim(bin_cns_gain)[1]))
+  mat <- as.matrix(mat)
+  mask <- as.matrix(bin_cns_gain[,c(1:dim(cns)[2])])
+  mat[!mask] <- NA
+  mat <- as.data.frame(mat) %>% tidyr::unite("names", everything(), sep = ',', na.rm = TRUE)
+  df$gain.samples <- mat$names
   df <- df %>% dplyr::filter((loss.freq > pL) | (gain.freq > pG),
                       (loss.count/ncol(cns) > prop) | (gain.count/ncol(cns) > prop),
                       (mean.cn < lowT) | (mean.cn > highT)
                       )
-  df <- collapse_table_to_segments(df)
   
+  # Collapse the data frame to genomic segments
+  df <- collapse_table_to_segments(df)
   df <- df %>% transform(chromosome = as.character(chromosome))
   df$chromosome[df$chromosome == 23] <- 'X'
   df$coordinates <- paste0('chr', df$chromosome, ':', as.character(df$start), '-', as.character(df$end))
   df$segment <- NULL
   
+  # Add genes present in the region to the output table
+  mycoords.gr = df %>% dplyr::select(chromosome, start, end) %>% 
+                    mutate(chromosome=paste0('chr', chromosome)) %>% 
+                    makeGRangesFromDataFrame
+  overlaps <- findOverlaps(genes(TxDb.Hsapiens.UCSC.hg19.knownGene, 
+                                 single.strand.genes.only=FALSE), mycoords.gr)
+  entrez_ids <- names(genes(TxDb.Hsapiens.UCSC.hg19.knownGene, 
+                            single.strand.genes.only=FALSE)[overlaps@from])
+  symbols <- getSYMBOL(entrez_ids, data='org.Hs.eg')
+  genes_table <- data.frame(symbols = symbols, entrez_id = entrez_ids, entry = overlaps@to) %>% 
+                      dplyr::filter(!is.na(symbols))
+  genes_table <- genes_table %>% 
+                    group_by(entry) %>% 
+                    summarise(features_in_region = paste0(symbols, collapse = ","))
+  df$entry <- 1:dim(df)[1]
+  df <- df %>% dplyr::left_join(genes_table, by = c('entry'))
+  df$entry <- NULL
+  
+  # Save the output table
   write.table(df, file = save_path, sep = '\t', col.names = TRUE, row.names = FALSE)
 }
 
 
 # Input
-ccnvFILT_obj <- readRDS(file = '~/Documents/projects/cn_signatures_shallowWGS/qdnaseq_copy_number/batch_1-13/Xchr_included/15kb_rCN_comCNVfilt.rds')
+ccnvFILT_obj <- readRDS(file = '~/Documents/projects/cn_sigs_swgs/copy_number_objects/Xchr_included/30kb_rCN_comCNVfilt.rds')
 # Output
-save_path <- '~/Documents/projects/cn_signatures_shallowWGS/data/summaryTables/NSMP_samples_batches1-13.tsv'
+save_path <- '~/Documents/projects/cn_sigs_swgs/data/summaryTables/NSMP_samples_batches1-13.tsv'
 # List of samples for which data is desired
 # Pay attention to the formatting! Its important!
 samples_of_interest <- c('CC-VGH-1176', 'CC-VGH-1187', 
